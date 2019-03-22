7868e69aa0bea1a039e6f81c69af9e90dbf3f0b3
Joseph Arias <arias.joseph.a@gmail.com>
2019-03-22 18:39:11 -0400

    escape-characters

---
+++ String-I/escape-characters.rb
@@ -0,0 +1,9 @@
+
+puts "Juliet said 'Goodbye' to Romero"
+puts 'Juliet said "Goodbye" to Romero'
+
+puts "Juliet said \"Goodbye\" to Romero"
+
+##############################################
+
+puts "Let's add a line break \nright here"
