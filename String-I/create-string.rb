39e63eb4b238160350c2745ef46e8bbe2020a836
Joseph Arias <arias.joseph.a@gmail.com>
2019-03-21 23:26:43 -0400

    Multiline String

--- String-I/create-string.rb
+++ String-I/create-string.rb
@@ -1,2 +1,18 @@
 puts "Hi there, I'm a string"
 puts "Hello"
+
+name = "Boris"
+revenue = "$23 dollars"
+puts name, revenue
+
+space = " "
+p space.length
+
+p name.class # String
+
+###########################################
+
+secondName = String.new("Joseph")
+puts secondName
+
+p 5.to_s # = "5"
---
+++ String-I/multiline-string.rb
@@ -0,0 +1,11 @@
+words = <<MLS
+  This will be a multiline string
+  when it is output
+
+  The tabs will also be preserved.
+  Just you watch
+  GoodBye
+
+MLS
+
+puts words
