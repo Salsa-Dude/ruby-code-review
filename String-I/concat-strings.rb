fd2aafaa0617454c438786078054a06415704492
Joseph Arias <arias.joseph.a@gmail.com>
2019-03-22 23:16:48 -0400

    concat string

---
+++ String-I/concat-strings.rb
@@ -0,0 +1,16 @@
+
+first_name = "Joseph "
+last_name = "Arias"
+
+puts first_name + last_name
+puts first_name += last_name
+
+# concat - it mutates the original string
+puts first_name.concat(" Alexis")
+
+# shovel method
+puts first_name << " lino"
+
+# prepend - puts in front of the string
+# also overwrites the original string
+puts first_name.prepend("King ")
