27a3168819317f118cf75158ae69cad19f11a33f
Joseph Arias <arias.joseph.a@gmail.com>
2019-03-22 22:15:42 -0400

    equality and inequality

---
+++ String-I/equality.rb
@@ -0,0 +1,19 @@
+a = "Hello"
+b = "hello"
+c = "Hello"
+
+puts a == b #False
+puts a == c #True
+
+###################################
+
+puts a != b #True
+
+####################################
+
+# apple appears first than banana
+puts "Apple" < "Banana" # True
+
+# Capital letters become before the lowercase letters
+puts "A" < "a"  # True
+puts "Help" < "banana" # True
