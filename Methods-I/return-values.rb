2d21304ec53f1cfb9f04829097f3bb9090cc55e9
Joseph Arias <arias.joseph.a@gmail.com>
2019-03-25 17:57:50 -0400

    return-values I

---
+++ Methods-I/return-values.rb
@@ -0,0 +1,8 @@
+
+def add_two_numbers(num1, num2)
+  puts "Okay i'm solving your math problems"
+  return num1 + num2  # explict return
+  # num1 + num2 #implict return
+end
+
+p add_two_numbers(3,5)
