af2eb458215fede82b083b896c810f6ccc2031cd
Joseph Arias <arias.joseph.a@gmail.com>
2019-03-25 14:09:39 -0400

    string case methods

---
+++ String-I/case-methods.rb
@@ -0,0 +1,9 @@
+
+puts "hello".capitalize  # Hello
+puts "hello world".capitalize  # Hello world
+
+## upcase method
+puts "hello".upcase  # HELLO
+
+## lowercase method
+puts 'My name is Joseph'.downcase  # my name is joseph
---
+++ String-I/overwrite-characters.rb
@@ -0,0 +1,9 @@
+
+fact = "I love blueberry pie"
+p fact
+
+fact[7,4] = "rasp"
+p fact  # I love raspberry pie
+
+fact[2..5] = "absolutely adore"
+p fact # "I absolutely adore raspberry pie"
