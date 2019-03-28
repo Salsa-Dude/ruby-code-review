b8f67b50ce87a9d1000c10db9cfb09dce887d1f3
Joseph Arias <arias.joseph.a@gmail.com>
2019-03-28 17:50:05 -0400

    review block

---
+++ Arrays-II/block.rb
@@ -0,0 +1,15 @@
+
+3.times { |num| puts "I am currenlty in loop #{num}"}
+# I am currenlty in loop 0
+# I am currenlty in loop 1
+# I am currenlty in loop 2
+
+5.times do |num|
+  square = num * num
+  puts "the current number is #{num} and its squared is #{square}"
+end
+# the current number is 0 and its squared is 0
+# the current number is 1 and its squared is 1
+# the current number is 2 and its squared is 4
+# the current number is 3 and its squared is 9
+# the current number is 4 and its squared is 16
