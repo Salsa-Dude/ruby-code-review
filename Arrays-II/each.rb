1eb621bfe437b44835b3578c3a637910d56802c9
Joseph Arias <arias.joseph.a@gmail.com>
2019-03-28 18:01:05 -0400

    each method

---
+++ Arrays-II/each.rb
@@ -0,0 +1,20 @@
+
+foods = ["pizza", "rice", "chicken"]
+
+foods.each do |food|
+  puts "I love eating #{food}"
+end
+# I love eating pizza
+# I love eating rice
+# I love eating chicken
+
+nums = [1, 2, 3, 4]
+
+nums.each do |num|
+  square = num * num
+  puts "the square of #{num} is #{square}"
+end
+# the square of 1 is 1
+# the square of 2 is 4
+# the square of 3 is 9
+# the square of 4 is 16
