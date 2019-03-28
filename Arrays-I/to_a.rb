0cee42bf07638268580ba17f5d4e595b8c99aca9
Joseph Arias <arias.joseph.a@gmail.com>
2019-03-28 15:08:39 -0400

    to_a

---
+++ Arrays-I/to_a.rb
@@ -0,0 +1,7 @@
+
+letters_range = "A".."J"
+p letters_range # "A".."J"
+p letters_range.to_a # ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+puts 1.is_a?(Array)  # false
+puts 1.is_a?(Integer)  # true
