ab76f4389cf373a8f6b6214851642b234e9c41be
Joseph Arias <arias.joseph.a@gmail.com>
2019-04-01 13:32:37 -0400

    concat

---
+++ Arrays-II/reverse.rb
@@ -0,0 +1,9 @@
+
+p [1,2,3].reverse # [3, 2, 1]
+
+orginial = [1,2,3]
+p orginial
+
+# using the reverse method with a bang method
+orginial.reverse!
+p orginial
---
+++ Arrays-II/sort.rb
@@ -0,0 +1,4 @@
+
+numbers = [1,6,3,8,4]
+
+p numbers.sort # [1, 3, 4, 6, 8]
