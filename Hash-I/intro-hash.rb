c97f35629e5fa49d8db84298f92f7a8c5d5341dc
Joseph Arias <arias.joseph.a@gmail.com>
2019-04-01 23:15:38 -0400

    intro-hash

---
+++ Hash-I/intro-hash.rb
@@ -0,0 +1,12 @@
+
+# What is a hash?
+
+# a hash is a data structure that stores key-value pairs like a dictonary
+
+nfl_roster = {
+  "Tom Brady" => "New England Patriots",
+  "Tomo Romo" => "Dallas Cowboys",
+  "Rob Gronkowski" => "New England Patriots"
+}
+
+p nfl_roster["Tomo Romo"] # "Dallas Cowboys"
